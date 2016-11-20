f34839d215d74e61b7d1c7d4cab3bcac04efe155
yang ming <yangming@yangdeMac.local>
2016-11-20 22:01:10 +0800

    Initial Commit

---
+++ UIVCSwitchByStoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIVCSwitchByStoryBoard
+//
+//  Created by yang ming on 2016/11/20.
+//  Copyright © 2016年 yang ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIVCSwitchByStoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIVCSwitchByStoryBoard
+//
+//  Created by yang ming on 2016/11/20.
+//  Copyright © 2016年 yang ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
