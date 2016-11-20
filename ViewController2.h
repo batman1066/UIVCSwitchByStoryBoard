bf4c6d2e85d30c69c9ae023f994dc8c8fb61c90e
yang ming <yangming@yangdeMac.local>
2016-11-20 22:27:46 +0800

    1

---
+++ ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  UIVCSwitchByStoryBoard
+//
+//  Created by yang ming on 2016/11/20.
+//  Copyright © 2016年 yang ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
--- ViewController2.m
+++ ViewController2.m
@@ -1,29 +1,38 @@
 //
-//  ViewController.m
+//  ViewController2.m
 //  UIVCSwitchByStoryBoard
 //
 //  Created by yang ming on 2016/11/20.
 //  Copyright © 2016年 yang ming. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "ViewController2.h"
 
-@interface ViewController ()
+@interface ViewController2 ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController2
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(nullable id)sender {
     NSLog(@"%s",__func__);
     NSLog(@"segue id:%@,src VC:%@, des VC %@ ",segue.identifier,
